7f28b033f2a8dd946336f030e4f817241b8b8302
Cazi <71891696+Cazi@users.noreply.github.com>
2024-10-04 10:36:21 -0400

    Completed the home page

--- eleven-percent/src/app/components/Header.tsx
+++ eleven-percent/src/app/components/Header.tsx
@@ -2,11 +2,11 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="sticky top-0 max-w-[1000px] mx-auto w-f flex items-center justify-between ">
-      <Link href='/' className="font bold text-xl transition ease-in-out hover:underline ">The Eleven Percent Project</Link>
+    <header className=" bg-[#d2af85] p-5 px-7 rounded-md sticky top-0 max-w-[1000px] mx-auto w-f flex items-center justify-between ">
+      <Link href='/' className="font-bold text-xl transition ease-in-out hover:underline ">The Eleven Percent Project</Link>
       <nav className="flex gap-5 *:py-2 *:px-4 *:rounded-md">
         <Link href='/about' className="bg-gray-200 transition ease-in-out hover:bg-gray-400">About</Link>
-        <Link href='/visuals' className="bg-blue-600 text-white transition ease-in-out hover:bg-sky-700">Visualization</Link>
+        <Link href='/visuals' className="bg-blue-600 text-white transition ease-in-out hover:bg-sky-800">Visualization</Link>
       </nav>
     </header>
   );
--- eleven-percent/src/app/page.tsx
+++ eleven-percent/src/app/page.tsx
@@ -1,18 +1,37 @@
 import Image from "next/image";
 
 import walk1 from "./public/walkout-1.jpg"
+import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <div className="flex justify-center mt-5">
+      <div className="flex justify-center mt-10">
         <Image
+          className="border-8 border-[#d2af85]"
           src={walk1}
           alt=""
           width={600}
           height={600}
         />
       </div>
-      <p className="text-center mt-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius dignissimos, sint iusto, nesciunt corrupti quidem quaerat natus quibusdam reprehenderit laboriosam cupiditate ex in delectus quam placeat rerum alias? Vitae, iusto!</p>
+      <p className="text-center mt-6 text-lg">
+        <u>
+          "In 1968, 65 of the 85 African American men and women enrolled at
+          Brown marched down College Hill to the Congdon Street Baptist Church to
+          protest the small number of black students admitted to the University as
+          well as a lack of support when they attended. At the time, only <b>2.3 percent </b>
+          of Brown students were African American; the protestors demanded a path to raise that number to
+          <b> 11 percent</b>, to match the U.S. population overall."
+        </u>
+        <Link
+          href={"https://www.brown.edu/about/history/timeline/black-student-walkout"}
+          className="transition ease-in-out hover:underline hover:font-bold"
+        >  
+          - Brown University, History
+        </Link>
+
+      </p>
+      <p className="text-center mt-2 font-extrabold"><u>This is the Eleven Percent Project.</u></p>
 
 
     </>
