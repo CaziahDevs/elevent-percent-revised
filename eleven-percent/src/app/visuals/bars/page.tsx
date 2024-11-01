c87ed729666d5750c1bd41ae4d289bd3fc807733
CaziahDevs <caziahcoding@gmail.com>
2024-11-01 17:41:16 -0400

    Added year labels back for the grad bar graph

--- eleven-percent/src/app/visuals/bars/page.tsx
+++ eleven-percent/src/app/visuals/bars/page.tsx
@@ -7,10 +7,7 @@ import UndergradBarGraph from "./graphs/UndergradBarGraph";
 
 import total_grad from '../../data/total-grad.json'
 
-const yearValues = [
-    1980, 1985, 1990, 1995, 2000, 2005, 2010, 2015, 2020, 2022
-];
-
+const years = [1980, 1984, 1986, 1988, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022];
 export default function Bars() {
     const [graph, setGraph] = useState('GradBar')
     let displayGraph;
@@ -22,10 +19,10 @@ export default function Bars() {
             displayGraph = <UndergradBarGraph />;
             break;
         case 'GradBar':
-            displayGraph = <GradBarGraph total_grad={total_grad} tickValues={yearValues} />;
+            displayGraph = <GradBarGraph total_grad={total_grad} tickValues={years} />;
             break;
         default:
-            displayGraph = <GradBarGraph total_grad={total_grad} tickValues={yearValues} />;
+            displayGraph = <GradBarGraph total_grad={total_grad} tickValues={years} />;
             break;
     }
 
