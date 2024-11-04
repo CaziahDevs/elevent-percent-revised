23b579abd1a009fd0fa2bb4b2dfbcce15763f4cd
CaziahDevs <caziahcoding@gmail.com>
2024-11-04 16:48:26 -0500

    Added axes to percentage graphs

--- eleven-percent/src/app/visuals/percents/graphs/GradPGraph.tsx
+++ eleven-percent/src/app/visuals/percents/graphs/GradPGraph.tsx
@@ -12,7 +12,7 @@ const GradPGraph: React.FC<Props> = ({ total_grad, tickValues }) => {
     <div className="relative z-0">
       <div className="w-full overflow-x-auto">
         <div className="min-w-[1500px] px-4n text-center">
-          <h3 className='text-lg underline'>Growth of the Total Black graduate Student Population vs Total Graduate Students</h3>
+          <h3 className='text-lg underline'>Growth of the Total Black Graduate Student Population vs Total Graduate Students</h3>
           <VictoryChart
             theme={VictoryTheme.clean}
             width={1500}
@@ -20,7 +20,37 @@ const GradPGraph: React.FC<Props> = ({ total_grad, tickValues }) => {
             domainPadding={{ x: 20 }}
             domain={{ x: [1980, 2022] }}
             padding={{ top: 50, bottom: 120, left: 120, right: 50 }}
-          ></VictoryChart>
+          >
+
+            <VictoryAxis
+              tickValues={tickValues}
+              style={{
+                tickLabels: {
+                  fontSize: 15,
+                  angle: -45,
+                  textAnchor: 'end',
+                  fontWeight: 'bold'
+                },
+                axisLabel: { fontSize: 20, padding: 80, fontWeight: 'bold' }
+              }}
+              label="Year"
+            />
+
+            <VictoryAxis
+              dependentAxis
+              style={{
+                tickLabels: {
+                  fontSize: 15,
+                  angle: -45,
+                  textAnchor: "end",
+                  padding: 5,
+                  fontWeight: 'bold'
+                },
+                axisLabel: { fontSize: 20, padding: 60, fontWeight: 'bold' }
+              }}
+              label="Number of Students"
+            />
+          </VictoryChart>
         </div>
       </div>
     </div>
